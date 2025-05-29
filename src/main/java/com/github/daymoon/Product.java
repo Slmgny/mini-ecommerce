b04329941a9c56e15e2279f9eeda3fd961d4fffb
Selim Günay <selimgnytr@gmail.com>
2025-05-29 21:41:08 +0300

    Adding Lists

--- src/main/java/com/github/daymoon/Product.java
+++ src/main/java/com/github/daymoon/Product.java
@@ -1,24 +1,36 @@
 package com.github.daymoon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Product {
 
+    private int id;
     private String name;
     private int price;
     private int sellerId;
     private int sellCount;
     private int stock;
     
-    public Product(String name, int price , int sellerId) {
+    public Product(String name, int price , int sellerId , int stock) {
         this.name = name;
         this.price = price;
         this.sellerId = sellerId;
+        this.stock = stock;
         AddToDataBase();
     }
     
+    public Product(int id,String name, int price, int sellerId, int sellCount, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.sellerId = sellerId;
+        this.sellCount = sellCount;
+        this.stock = stock;
+        AddToDataBase();
+    }
+
+
 
     //Getter and Setters
     public String getName() {
@@ -56,6 +68,32 @@ public class Product {
         sellCount++;
     }
 
+    public ArrayList<Product> getAllProducts() {
+        ArrayList<Product> products = new ArrayList<>();
+
+        String url = "jdbc:sqlite:C:\\Java\\mini-ecommerce\\DataBase\\mini-ecommerce-database.db";
+        String sql = "SELECT name, price, stock, sell_count FROM Products";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                int stock = rs.getInt("stock");
+                int sellCount = rs.getInt("sell_count");
+
+                Product product = new Product(id,name, price, stock, sellCount);
+                products.add(product);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
 
     public void AddToDataBase(){
 
