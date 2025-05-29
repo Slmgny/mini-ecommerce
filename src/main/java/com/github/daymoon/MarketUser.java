76a05fcca3a1fb00d3eb1a9f32c3e75c03171bde
Selim Günay <selimgnytr@gmail.com>
2025-05-29 19:39:17 +0300

    4

--- src/main/java/com/github/daymoon/MarketUser.java
+++ src/main/java/com/github/daymoon/MarketUser.java
@@ -11,4 +11,23 @@ public class MarketUser extends User {
     public MarketUser(String name , String password){
         super(name , password);
     }
+
+    public void addToCart(Product p){
+        cart.add(p);
+    }
+
+    public void addToMarket(Product p){
+        marketProducts.add(p);
+    }
+
+    public void addToPurchaseHistory(Product p){
+        purchaseHistory.add(p);
+    }
+
+    public void ShowCart(){
+        for(Product p: cart){
+            System.out.println(p.getName() + "                " + p.getPrice());
+        }
+    }
+
 }
--- src/main/java/com/github/daymoon/Product.java
+++ src/main/java/com/github/daymoon/Product.java
@@ -1,7 +1,50 @@
 package com.github.daymoon;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class Product {
+
     private String name;
     private int price;
     private int id;
+    
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+        AddToDataBase();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void AddToDataBase(){
+
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Java\\mini-ecommerce\\DataBase\\mini-ecommerce-database.db"){
+            String sql = "INSERT INTO Products(name , price) VALUES(?, ?)";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, this.name);
+            pst.setInt(2, this.price);
+            pst.executeUpdate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    
 }
